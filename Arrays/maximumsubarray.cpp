f10644594670e3f5c08b7d1ccfbea84d8209ab0a
Sravankumar <kumarsravan098@gmail.com>
2025-08-01 22:57:47 +0530

    Initial commit

--- Arrays/maximumsubarray.cpp
+++ Arrays/maximumsubarray.cpp
@@ -4,25 +4,21 @@ using namespace std;
 
 int main(){
     vector<int> arr={3,-4,5,4,-1,7,-8};
-    cout<<arr.size()<<endl;
-    int sum=0,maxsum=0,start=-1,end=-1;
-    for(int i=0;i<=arr.size();i++){
-        for(int j=i;j<=arr.size();j++){
-            for(int k=i;k<j;k++){
-                cout<<arr[k];
-                sum+=arr[k];
-            }
-            if(sum>maxsum){
-                start=i;end=j-1;
-                maxsum=max(sum,maxsum);
-                
-            }
-            sum=0;
-            cout<<" ";
+    cout<<"size of the array : "<<arr.size()<<endl;
+    int sum=0,maxsum=0,start=0,end=0;
+    for(int i=0;i<arr.size();i++){
+        sum+=arr[i];
+       
+        if(sum>maxsum){
+            maxsum=sum;
+            end=i;              //KADANES ALGORITHM  ... WHEN EVER THE SUM REACHES -1 SIMPLY SET IT TO 0
         }
-        cout<<endl;
+         if(sum<0){
+            sum=0;             //WRITING THIS CASE AFTER CHECKING FOR THE MAXIMUM IN ORDER TO  FIND THE GREATEST IN A COMPLETE NEGATIVE ELEMENTS ARRAY
+            start=i+1;
+        }
     }
     cout<<start<<" "<<end<<endl;
-    cout<<maxsum;
+    cout<<"maximum sum : "<<maxsum;
 
 }
